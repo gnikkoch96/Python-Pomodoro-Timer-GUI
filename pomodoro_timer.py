6ea3bdc56810ce12de49a7617eba0c1f83f6dccd
Nikko <56482580+gnikkoch96@users.noreply.github.com>
2021-09-07 14:40:23 -0700

    implemented threading to udpate GUI

--- pomodoro_timer.py
+++ pomodoro_timer.py
@@ -1,11 +1,13 @@
 import time
+import threading
+
 
 # Pomodoro_Timer used to handle/create the GUI for the timer
 class pomodoro_timer():
     def __init__(self, dpg, settings):
         self.dpg = dpg
         self.settings = settings
-        self.timer = Timer() # starts the timer as soon as the gui is generated
+        self.timer = Timer(self.settings.get_focus_time())  # starts the timer as soon as the gui is generated
 
         with dpg.window(label="Pomdoro Timer",
                         height=self.dpg.get_viewport_height(),
@@ -14,11 +16,12 @@ class pomodoro_timer():
             self.create_buttons()
 
 
+
     def create_displays(self):
-        displayMinTime = self.dpg.add_input_text(label="min", default_value=self.settings.get_focus_time())
+        displayMinTime = self.dpg.add_input_text(label="min", default_value=self.timer.get_min_value())
         self.dpg.configure_item(displayMinTime, enabled=False)
 
-        displaySecTime = self.dpg.add_input_text(label="sec", default_value=0)
+        displaySecTime = self.dpg.add_input_text(label="sec", default_value=self.timer.get_sec_value())
         self.dpg.configure_item(displaySecTime, enabled=False)
 
     def create_buttons(self):
@@ -31,7 +34,6 @@ class pomodoro_timer():
         buttonRestart = self.dpg.add_button(label="Restart", id="Restart", callback=self.restart_callback)
         self.dpg.add_same_line()
         buttonSettings = self.dpg.add_button(label="Settings", id="Settings", callback=self.settings_callback)
-
 
     def settings_callback(self):
         # 1. destroys the pomodoro timer window
@@ -58,24 +60,26 @@ class pomodoro_timer():
 
 # Timer class used to handle the timer functionalities
 class Timer:
-    # initialize the timer with a min
-    def __init__(self, min):
+    # initialize the timer with mins
+    def __init__(self, mins):
         # initialize values
-        self.min = min
+        self.mins = int(mins)
         self.sec = 0
-        self.continue_timer = True # bool used to control the timer
-        self.start_timer()
+        self.continue_timer = True  # bool used to control the timer
+        threading.Thread(target=self.start_timer).start()
 
     def start_timer(self):
         # continue the timer until user stops, pauses, or restarts the timer
         # and when the min counter >= 0 and sec is greater than 0
-        while self.continue_timer || (self.min >= 0 and self.sec > 0):
+        while self.continue_timer and (self.mins >= 0 and self.sec >= 0):
             if self.sec <= 0:
-                self.min -= self.min - 1
+                self.mins -= 1
                 self.sec = 60
             else:
                 self.sec -= 1
                 time.sleep(1)
+
+            print(self.mins, " mins\n", self.sec, " secs")
 
     def restart_timer(self):
         self.continue_timer = False;
@@ -86,9 +90,8 @@ class Timer:
     def pause_timer(self):
         self.continue_timer = False;
 
-
     def get_min_value(self):
-        pass
+        return self.mins
 
     def get_sec_value(self):
-        pass
+        return self.sec

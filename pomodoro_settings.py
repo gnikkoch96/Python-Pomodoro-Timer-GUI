b44ca83689a89cb9b7a1cc75a7c53f0c672dfb3c
Nikko <56482580+gnikkoch96@users.noreply.github.com>
2021-09-07 11:53:20 -0700

    updated gui and implemented more features

--- pomodoro_settings.py
+++ pomodoro_settings.py
@@ -1,18 +1,46 @@
+from pomodoro_timer import pomodoro_timer
+
 
 class pomodoro_settings():
     def __init__(self, dpg):
-        with dpg.window(label="Pomodoro Timer Settings", width=300):
-            self.inputFocusTimeInt = dpg.add_input_int(label="Focus Time", default_value=25)
-            self.inputSmallBreakInt = dpg.add_input_int(label="Small Break", default_value=2)
-            self.inputLongBreakInt = dpg.add_input_int(label="Long Break", default_value=15)
+        self.dpg = dpg
+        self.list_time = self.create_time_list()
+
+        with dpg.window(label="Pomodoro Timer Settings",
+                        height=self.dpg.get_viewport_height(),
+                        width=self.dpg.get_viewport_width()):
+            self.comboFocusTime = dpg.add_combo(label="Focus Time",
+                                                id="Focus Time Combo",
+                                                items=self.list_time,
+                                                default_value=25)
+            self.comboSmallBreak = dpg.add_combo(label="Small Break",
+                                                 id="Small Break Combo",
+                                                 items=self.list_time,
+                                                 default_value=2)
+            self.comboLongBreak = dpg.add_combo(label="Long Break",
+                                                id="Long Break Combo",
+                                                items=self.list_time,
+                                                default_value=15)
+            self.buttonStart = dpg.add_button(label="Start Pomodoro!",
+                                              id="Start",
+                                              callback=self.start_button_callback)
 
             # dpg.configure_item(inputFocusTimeInt, enabled=False)
 
+    def start_button_callback(self):
+        pTimer = pomodoro_timer(self.dpg, self)
+
     def get_focus_time(self):
-        return self.inputFocusTimeInt
+        return self.dpg.get_value("Focus Time Combo")
 
     def get_small_break(self):
-        return self.inputSmallBreakInt
+        return self.dpg.get_value("Small Break Combo")
 
     def get_long_break(self):
-        return self.inputLongBreakInt
+        return self.dpg.get_value("Long Break Combo")
+
+    def create_time_list(self):
+        time_list = []
+        for i in range(1, 61):  # creates a list between 1 - 60 mins to choose for the time
+            time_list.append(i)
+        return time_list
--- pomodoro_timer.py
+++ pomodoro_timer.py
@@ -1,19 +1,94 @@
+import time
 
+# Pomodoro_Timer used to handle/create the GUI for the timer
 class pomodoro_timer():
     def __init__(self, dpg, settings):
         self.dpg = dpg
         self.settings = settings
+        self.timer = Timer() # starts the timer as soon as the gui is generated
 
-        with dpg.window(label="Pomdoro Timer", height=100, width=300):
+        with dpg.window(label="Pomdoro Timer",
+                        height=self.dpg.get_viewport_height(),
+                        width=self.dpg.get_viewport_width()):
             self.create_displays()
             self.create_buttons()
 
 
     def create_displays(self):
-        displayTime = self.dfg.add_input_text(label="Time Left", default_value=self.settings.get)
+        displayMinTime = self.dpg.add_input_text(label="min", default_value=self.settings.get_focus_time())
+        self.dpg.configure_item(displayMinTime, enabled=False)
+
+        displaySecTime = self.dpg.add_input_text(label="sec", default_value=0)
+        self.dpg.configure_item(displaySecTime, enabled=False)
 
     def create_buttons(self):
-        buttonPause = self.dpg.add_button(label="Pause", id="Pause")
-        buttonResume = self.dpg.add_button(label="Resume", id="Resume")
-        buttonStop = self.dpg.add_button(label="Stop", id="Stop")
-        buttonRestart = self.dpg.add_button(label="Restart", id="Restart")
+        buttonPause = self.dpg.add_button(label="Pause", id="Pause", callback=self.pause_callback)
+        self.dpg.add_same_line()
+        buttonResume = self.dpg.add_button(label="Resume", id="Resume", callback=self.resume_callback)
+        self.dpg.add_same_line()
+        buttonStop = self.dpg.add_button(label="Stop", id="Stop", callback=self.stop_callback)
+        self.dpg.add_same_line()
+        buttonRestart = self.dpg.add_button(label="Restart", id="Restart", callback=self.restart_callback)
+        self.dpg.add_same_line()
+        buttonSettings = self.dpg.add_button(label="Settings", id="Settings", callback=self.settings_callback)
+
+
+    def settings_callback(self):
+        # 1. destroys the pomodoro timer window
+        # 2. launches the settings window
+        pass
+
+    def pause_callback(self):
+        # 1. pauses the timer
+        pass
+
+    def resume_callback(self):
+        # 1. resume the timer
+        pass
+
+    def stop_callback(self):
+        # 1. resets the timer of focus
+        pass
+
+    def restart_callback(self):
+        # 1. resets the timer back to focus time
+        # 2. resets the counter for the pomodoro cycle
+        pass
+
+
+# Timer class used to handle the timer functionalities
+class Timer:
+    # initialize the timer with a min
+    def __init__(self, min):
+        # initialize values
+        self.min = min
+        self.sec = 0
+        self.continue_timer = True # bool used to control the timer
+        self.start_timer()
+
+    def start_timer(self):
+        # continue the timer until user stops, pauses, or restarts the timer
+        # and when the min counter >= 0 and sec is greater than 0
+        while self.continue_timer || (self.min >= 0 and self.sec > 0):
+            if self.sec <= 0:
+                self.min -= self.min - 1
+                self.sec = 60
+            else:
+                self.sec -= 1
+                time.sleep(1)
+
+    def restart_timer(self):
+        self.continue_timer = False;
+
+    def stop_timer(self):
+        self.continue_timer = False;
+
+    def pause_timer(self):
+        self.continue_timer = False;
+
+
+    def get_min_value(self):
+        pass
+
+    def get_sec_value(self):
+        pass

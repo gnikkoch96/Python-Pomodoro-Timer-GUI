3e26cd0921c45e5ac8346a274013ac7343cd2efd
Nikko Chan <56482580+gnikkoch96@users.noreply.github.com>
2022-01-07 13:51:17 -0800

    'save'

--- testing/dynamic-texture-test.py
+++ testing/dynamic-texture-test.py
@@ -3,8 +3,6 @@ import os
 import threading
 import time
 
-
-
 pkg_dir = os.path.dirname(os.path.abspath(__file__))
 pic1 = os.path.join(pkg_dir, "../resources/images/relaxing.gif")
 pic2 = os.path.join(pkg_dir, "../resources/images/relaxing - Copy.gif")
@@ -14,12 +12,12 @@ width, height, channels, texture_data = dpg.load_image(pic1)
 with dpg.texture_registry():
     dpg.add_dynamic_texture(width, height, texture_data, id="texture_id")
 
-
 with dpg.window(label="Tutorial"):
     dpg.add_image("texture_id")
 
+
 def update_image():
-    while(True):
+    while (True):
         time.sleep(1)
         print("hello")
         width, height, channels, texture_data = dpg.load_image(pic2)
@@ -27,6 +25,7 @@ def update_image():
         time.sleep(1)
         width, height, channels, texture_data = dpg.load_image(pic1)
         dpg.set_value("texture_id", texture_data)
+
 
 threading.Thread(target=update_image, daemon=True).start()
 dpg.start_dearpygui()
